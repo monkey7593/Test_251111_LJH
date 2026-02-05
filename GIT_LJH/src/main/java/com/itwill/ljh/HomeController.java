a28a8897323d0ccd2f188fe91161c650e26544a3
ITWILL <ITWILL@ITWILL5-2>
2026-02-05 11:14:18 +0900

    12

--- GIT_LJH/src/main/java/com/itwill/ljh/HomeController.java
+++ GIT_LJH/src/main/java/com/itwill/ljh/HomeController.java
@@ -26,6 +26,7 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		
 		
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
